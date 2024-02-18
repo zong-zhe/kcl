948b8f5989f0a3841702f1d73e259692d4057820
zong-zhe <zongzhe1024@163.com>
2024-02-18 23:40:27 +0800

    feat: add builtin io function abs()
    Signed-off-by: zong-zhe <zongzhe1024@163.com>

--- kclvm/runtime/src/file/mod.rs
+++ kclvm/runtime/src/file/mod.rs
@@ -22,3 +22,25 @@ pub extern "C" fn kclvm_file_read(
 
     panic!("read() takes exactly one argument (0 given)");
 }
+
+#[no_mangle]
+#[runtime_fn]
+pub extern "C" fn kclvm_file_abs(
+    ctx: *mut kclvm_context_t,
+    args: *const kclvm_value_ref_t,
+    _kwargs: *const kclvm_value_ref_t,
+) -> *const kclvm_value_ref_t {
+    let args = ptr_as_ref(args);
+    let ctx = mut_ptr_as_ref(ctx);
+
+    if let Some(x) = args.arg_i_str(0, None) {
+        match fs::canonicalize(x.to_string()) {
+            Ok(abs_path) => {
+                return ValueRef::str(abs_path.to_str().expect("failed to convert path to str")).into_raw(ctx);
+            },
+            Err(e) => panic!("failed to access the file in {}", x),
+        }
+    }
+
+    panic!("abs() takes exactly one argument (0 given)");
+}
